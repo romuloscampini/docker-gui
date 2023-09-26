3930a5c1f92273aac5c78f9cf497c401d671dfe0
Romulo Scampini <romulo@scampini.com.br>
2023-09-26 09:31:05 -0300

    fix: typo on docker image name

--- run.sh
+++ run.sh
@@ -14,4 +14,4 @@ docker build -t romuloscampini/docker-gui .
 defaults write org.xquartz.X11 nolisten_tcp -boolean false
 xhost + 127.0.0.1
 
-docker run --rm -it --name terminator -e DISPLAY=host.docker.internal:0 -v/tmp/.X11-unix:/tmp/.X11-unix docker-gui terminator
+docker run --rm -it --name terminator -e DISPLAY=host.docker.internal:0 -v/tmp/.X11-unix:/tmp/.X11-unix romuloscampini/docker-gui terminator
